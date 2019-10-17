b26b24217be440d2abc5343fc754bc55488f984d
Huy Trieu Duc <huy.trieu@orientsoftware.com>
2019-10-17 18:26:32 +0700

    fix something

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -20,6 +20,7 @@ namespace DatingApp.API.Controllers
             _context=context;
         }
         [Authorize]
+        [AllowAnonymous]
         [HttpGet]
         public async  Task<IActionResult> GetValues()
         {
